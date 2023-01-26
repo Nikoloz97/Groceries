f13c3a2268d2e2c2b6916c94293a8b0d269532e9
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-26 16:20:42 -0500

    Created separate files for product and shopping cart classes

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefactorDemo
+{
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public decimal Price;
+    }
+}
