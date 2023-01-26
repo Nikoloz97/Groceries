a6431c07eb8a0eba7a6dfdb3c99dfefe871b76dd
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-25 22:03:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace RefactorDemo
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddProduct("Milk");
+            shoppingCart.AddProduct("Eggs");
+            Console.WriteLine("Total Price: {0}", shoppingCart.GetCheckoutPrice());
+        }
+    }
+
+
+    public class ShoppingCart
+    {
+        private List<Product> _products = new List<Product>();
+
+        public void AddProduct(string name)
+        {
+            string dbPath = new DirectoryInfo(@"..\..").FullName + @"\GroceryStore.mdf";
+            string connectionString = string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={0}", dbPath);
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(string.Format("SELECT * FROM Product WHERE Name = '{0}'", name), conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            Product p = null;
+
+            if (dr.Read())
+            {
+                _products.Add(new Product
+                {
+                    Id = dr.GetFieldValue<int>("Id"),
+                    Name = dr.GetFieldValue<string>("Name"),
+                    Price = dr.GetFieldValue<decimal>("Price")
+                });
+            }
+        }
+
+        public decimal GetCheckoutPrice()
+        {
+            decimal price = 0;
+
+            foreach (Product p in _products)
+                price += p.Price;
+
+            return price;
+        }
+    }
+
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public decimal Price;
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# Refactor Demo Instructions
+
+This code represents some tight coupling and poor programming practices. We would like you to do some refactoring and code clean-up.
+
+Examples of refactors you could make include (this is not an exhaustive list): 
+	-Implementing a data repository and using dependency injection to provide it where needed.
+	-Altering the AddProduct() method to take in an object.
+	-Ensuring that objects are properly cleaned up where appropriate.
+	-Implementing defensive coding checks to provide feedback to the user.
+	-Making the GetCheckoutPrice() method in the ShoppingCart class testable by a unit test.
+	-BONUS: Write a unit test against the GetCheckoutPrice() method without accessing the database.
+	
+The overall objective of this exercise is to gauge your level of understanding and creativity. If you aren't sure how to act on some of the suggestions that is ok. If you find other things you want to change to make the code cleaner and easier to maintain feel free to do so.
